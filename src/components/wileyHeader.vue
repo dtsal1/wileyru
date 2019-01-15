7a4dd6230d731e8485d0b7e1f79b2c5f6be60abd
dtsal1 <dtsal@wiley.com>
2019-01-15 17:16:17 +0300

    added styles for navigationBar

--- src/components/wileyHeader.vue
+++ src/components/wileyHeader.vue
@@ -2,10 +2,10 @@
   <div id="navigationBar">
     <ul>
       <li><a href="" style="color:#D5410B">Продукты</a></li>
-      <li><a href="./about_wiley.html">Работа в компании</a></li>
-      <li><a href="./Career.html">О компании</a></li>
-      <li><a href="./Contacts.html">Связаться с нами</a></li>
-      <li><a href="./Contacts.html">wiley.com</a></li>
+      <li><a href="">Работа в компании</a></li>
+      <li><a href="">О компании</a></li>
+      <li><a href="">Связаться с нами</a></li>
+      <li><a href="">wiley.com</a></li>
     </ul>
 
   </div>
@@ -18,5 +18,19 @@
 </script>
 
 <style scoped>
+  #navigationBar{
+    border-bottom-width: 1px;
+    border-bottom-style: solid;
+    border-bottom-color: #000;
+    position:relative;
+    height: 150px;
+    margin-bottom:20px;}
+/*
+  .topnav-inner-wrap{width:970px;margin:0 auto;position:relative;}
+  #topnav #wordmark{float:left;}
+  #topnav #rusname {font-family:"Book Antiqua"; float:left; width: 508px; height: 33.39px; margin-left: 120px; margin-top: -1px;}
+  #topnav-header{width:inherit;margin-top:0;margin-bottom:10px;overflow:hidden;float:right;}
+*/
+
 
 </style>
